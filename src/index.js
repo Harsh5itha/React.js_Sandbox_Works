a1e87f7572af9ef9a0235a39d087d2dedab017e1
Harshitha T <45786603+Harsh5itha@users.noreply.github.com>
2021-11-01 17:23:31 +0530

    JSX and ES6

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const fName = "Harshitha";
+const lName = "T";
+const num = 9;
+
 ReactDOM.render(
   <div>
-    <h1>My Favourite Food</h1>
-    <ul>
-      <li>Pizza</li>
-      <li>Noodles</li>
-      <li>Jamon</li>
-    </ul>
+    <h1>Hello {fName + " " + lName}!</h1>
+    <p>Your lucky number is {num}</p>
   </div>,
   document.getElementById("root")
 );
