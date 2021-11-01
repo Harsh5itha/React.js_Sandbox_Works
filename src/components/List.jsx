95ab8d6214df5e341f2849bc7ffb8a5f5db64b82
Harshitha T <45786603+Harsh5itha@users.noreply.github.com>
2021-11-01 21:33:49 +0530

    React components

---
+++ src/components/List.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li>Pizza</li>
+      <li>Jamoon</li>
+      <li>Noodles</li>
+    </ul>
+  );
+}
+
+export default List;
